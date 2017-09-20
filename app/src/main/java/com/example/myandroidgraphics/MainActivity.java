67ad24071327b59bb14af3bf505311deb8f3f9e2
DarushDev <darushfaqiri@gmail.com>
2017-09-20 23:11:21 +0800

    Create wave and shimmer activities and layouts

--- app/src/main/java/com/example/myandroidgraphics/MainActivity.java
+++ app/src/main/java/com/example/myandroidgraphics/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.myandroidgraphics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnWave, btnShimmer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnWave = (Button) findViewById(R.id.btnWave);
+        btnShimmer = (Button) findViewById(R.id.btnShimmer);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnWave:
+
+                break;
+            case R.id.btnShimmer:
+
+                break;
+        }
     }
 }
